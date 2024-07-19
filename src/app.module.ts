5ad5b82199314698c5de274a573da3069f647aef
Oleksandr Krasnoshchokov <krasnoshchokovoleksandr@gmail.com>
2024-07-19 12:54:02 +0300

    texh-stack resource is init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TechStackModule } from './tech-stack/tech-stack.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TechStackModule,
   ],
   controllers: [AppController],
   providers: [AppService],
